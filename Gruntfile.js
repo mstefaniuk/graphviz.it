753aecaf20b942a88c7a34a351150e6db64d5a82
mstefaniuk <marcin@stefaniuk.info>
2015-11-11 20:27:17 +0100

    Fixes of publication task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
     'gh-pages': {
       'gh-pages': {
         options: {
-          base: 'build',
+          base: 'dist',
           push: false
         },
         src: ['**']
@@ -100,6 +100,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', ['build']);
   grunt.registerTask('start', ['connect:server', 'pouchdb']);
