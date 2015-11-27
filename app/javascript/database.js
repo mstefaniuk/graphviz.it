5d5fe65ce1b4202e6176d11e17a71bb5fe8e40fc
mstefaniuk <marcin@stefaniuk.info>
2015-11-27 23:24:39 +0100

    Database middleware for saving diagram as png image.

--- app/javascript/database.js
+++ app/javascript/database.js
@@ -1,13 +1,13 @@
-define(['pouchdb', 'config'], function(PouchDB, config) {
+define(['pouchdb', 'config'], function (PouchDB, config) {
 
   var db = new PouchDB(config.db.public);
 
   return {
     middleware: {
-      save: function(req, event, next) {
+      save: function (req, event, next) {
         var document = req.document || {
-          type: "fiddle"
-        };
+            type: "fiddle"
+          };
         db.put(document, generateShurl(8))
           .then(function (result) {
             req.params.fiddle = result.id;
@@ -16,21 +16,28 @@ define(['pouchdb', 'config'], function(PouchDB, config) {
             next();
           });
       },
-      extract: function(req, event, next) {
-        req.params.attachment = "" + (currentAttachment(req.document._attachments)+1);
+      extract: function (req, event, next) {
+        req.params.attachment = "" + (currentAttachment(req.document._attachments) + 1);
         req.params.fiddle = req.document._id;
         req.revision = req.document._rev;
         next();
       },
-      update: function(req, event, next) {
+      update: function (req, event, next) {
         var attachment = new Blob([req.source], {type: 'text/plain'});
         db.putAttachment(req.params.fiddle, req.params.attachment + '.gv', req.revision, attachment, 'text/plain')
           .then(function () {
             next();
           });
       },
-      load: function(req, event, next) {
-        db.get(req.params.fiddle).then(function(document) {
+      picture: function (req, event, next) {
+        var picture = dataURItoBlob(req.picture);
+        db.putAttachment(req.params.fiddle, req.params.attachment + '.png', req.revision, attachment, 'image/png')
+          .then(function () {
+            next();
+          });
+      },
+      load: function (req, event, next) {
+        db.get(req.params.fiddle).then(function (document) {
           req.document = document;
           if (!req.params.attachment) {
             req.params.attachment = "" + currentAttachment(document._attachments);
@@ -38,10 +45,10 @@ define(['pouchdb', 'config'], function(PouchDB, config) {
           next();
         });
       },
-      source: function(req, event, next) {
+      source: function (req, event, next) {
         db.getAttachment(req.document._id, req.params.attachment + '.gv').then(function (blob) {
           var reader = new FileReader();
-          reader.addEventListener("loadend", function() {
+          reader.addEventListener("loadend", function () {
             req.source = this.result;
             next();
           });
@@ -63,9 +70,28 @@ define(['pouchdb', 'config'], function(PouchDB, config) {
   function generateShurl(length) {
     var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     var shurl = '';
-    for (var i=0; i<length; i++) {
+    for (var i = 0; i < length; i++) {
       shurl += tab.charAt(Math.floor(Math.random() * tab.length));
     }
     return shurl;
+  }
+
+  function dataURItoBlob(dataURI) {
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+    else
+      byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type: mimeString});
   }
 });
