121f7c8267f6a959c03ddd1521c44f4525bfa856
mstefaniuk <marcin@stefaniuk.info>
2015-12-28 21:40:00 +0100

    Double check of Google analytics script.

--- app/javascript/analytics.js
+++ app/javascript/analytics.js
@@ -1,5 +1,9 @@
 define(['ga'], function (ga) {
-  ga.l = new Date();
-  ga('create', 'UA-68678128-1', 'auto');
-  return ga;
+  if (ga===undefined) {
+    return function() {};
+  } else {
+    ga.l = new Date();
+    ga('create', 'UA-68678128-1', 'auto');
+    return ga;
+  }
 });
